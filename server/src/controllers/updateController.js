962e607035a10de08399608fe01751fbfc087e37
Bhaswanth Polamarasetty <bhaswanth0526@gmail.com>
2025-02-16 22:57:36 +0530

    Added await

--- server/src/controllers/updateController.js
+++ server/src/controllers/updateController.js
@@ -1,30 +1,32 @@
 const Transactions = require("../models/transactions");
 
 exports.updateTransaction = async (req, res) => {
-    try{
+    try {
         const { date, transId, amount, desc, type, method } = req.body;
 
-        const day = Transactions.findOne({date});
+        const day = await Transactions.findOne({ date });
 
-        if(!day) return res.status(400).json({message: "No transactions available on this day."});
+        if (!day) return res.status(400).json({ message: "No transactions available on this day." });
 
-        const indx = day.transactions.findIndex(transc => transc._id.toString() == transId);
+        const indx = day.transactions.findIndex(transc => transc._id.toString() === transId);
+
+        if (indx === -1) return res.status(404).json({ message: "Transaction not found." });
 
         await Transactions.updateOne(
-            {date, [`transactions._id`]:transId},
+            { date, "transactions._id": transId },
             {
                 $set: {
-                    [`transactions.amount`] : amount,
-                    [`transactions.desc`]:desc,
-                    [`transactions.type`]:type,
-                    [`transactions.method`]:method
+                    "transactions.$.amount": amount,
+                    "transactions.$.desc": desc,
+                    "transactions.$.type": type,
+                    "transactions.$.method": method
                 }
             }
         );
 
-        res.status(200).json({message: "Transaction Update successfull."});
-    }catch(err) {
-        res.status(500).json({message: "Server error."});
-        console.log("Error occured from updateController.js.." + err);
+        res.status(200).json({ message: "Transaction update successful." });
+    } catch (err) {
+        res.status(500).json({ message: "Server error." });
+        console.log("Error occurred in updateTransaction.js: " + err);
     }
-}
+};
