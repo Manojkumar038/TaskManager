ab20e570b5687e8333547ca6326338f76320b529
Bhaswanth Polamarasetty <bhaswanth0526@gmail.com>
2025-02-17 02:20:28 +0530

    modified the model from date with array of transactions to normal transaction

--- server/src/models/Transaction.js
+++ server/src/models/Transaction.js
@@ -2,20 +2,16 @@ const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
     date: { type: Date, required: true },
-    transactions: [
-        {
-            amount: { type: Number, required: true },
-            desc: { type: String },
-            type: { type: String, enum: ['Income', 'Expense'], required: true },
-            category: { 
-                type: String, 
-                enum: ['Food', 'EMI', 'Fuel', 'Shopping', 'Entertainment', 'Bills', 'Travel', 'Health', 'Education', 'Savings', 'Investment', 'Other'], 
-                required: true 
-            },
-            customCategory: { type: String, required: function() { return this.category === 'Other'; } },
-            method: { type: String, enum: ['Cash', 'Card', 'UPI', 'Bank Transfer'], required: true }
-        }
-    ]
+    amount: { type: Number, required: true },
+    desc: { type: String },
+    type: { type: String, enum: ['Income', 'Expense'], required: true },
+    category: { 
+        type: String, 
+        enum: ['Food', 'EMI', 'Fuel', 'Shopping', 'Entertainment', 'Bills', 'Travel', 'Health', 'Education', 'Savings', 'Investment', 'Other'], 
+        required: true 
+    },
+    customCategory: { type: String, required: function() { return this.category === 'Other'; } },
+    method: { type: String, enum: ['Cash', 'Card', 'Credit Card', 'UPI', 'Bank Transfer'], required: true },
 }, { timestamps: true });
 
 const Transactions = mongoose.model('Transactions', transactionSchema);
