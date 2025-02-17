6dbd03bf257315c5ba706ca89c5b7e0b0eaea009
Manoj Kumar <18038mechmanojkumar@gmail.com>
2025-02-17 21:44:20 +0530

    modified Transactions model - added wants in category and changed Income - Credit and Expense - Debit

--- server/src/models/Transaction.js
+++ server/src/models/Transaction.js
@@ -4,14 +4,14 @@ const transactionSchema = new mongoose.Schema({
     date: { type: Date, required: true },
     amount: { type: Number, required: true },
     desc: { type: String },
-    type: { type: String, enum: ['Income', 'Expense'], required: true },
+    type: { type: String, enum: ['Credit', 'Debit'], required: true },
     category: { 
         type: String, 
-        enum: ['Food', 'EMI', 'Fuel', 'Shopping', 'Entertainment', 'Bills', 'Travel', 'Health', 'Education', 'Savings', 'Investment', 'Other'], 
+        enum: ['Food', 'EMI', 'Fuel', 'Shopping', 'Entertainment', 'Bills', 'Travel', 'Health', 'Education', 'Savings', 'Investment', 'Wants'], 
         required: true 
     },
     customCategory: { type: String, required: function() { return this.category === 'Other'; } },
-    method: { type: String, enum: ['Cash', 'Card', 'Credit Card', 'UPI', 'Bank Transfer'], required: true },
+    method: { type: String, enum: ['Cash', 'Card', 'UPI', 'Bank Transfer'], required: true },
 }, { timestamps: true });
 
 const Transactions = mongoose.model('Transactions', transactionSchema);
